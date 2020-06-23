3521260ded65be55d46fb36b3e7414cc5c5d820a
Shark <sharkbit@hotmail.com>
2020-06-22 20:21:03 -0400

    Update main.php
    FIxed Cookie Issue

--- backend/config/main.php
+++ backend/config/main.php
@@ -23,7 +23,7 @@ return [
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true, 'sameSite'=>'strict' ],
         ],
         'session' => [
             // this is the name of the session cookie used for login on the backend
