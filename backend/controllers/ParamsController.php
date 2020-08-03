2b10aab108b00ee0251e0aaaf19820d5dfcdda4f
Shark <sharkbit@hotmail.com>
2020-08-03 14:03:55 -0400

    Update ParamsController.php

--- backend/controllers/ParamsController.php
+++ backend/controllers/ParamsController.php
@@ -32,8 +32,6 @@ class ParamsController extends AdminController {
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post())) {
-			yii::$app->controller->createLog(false, 'trex', var_export($model->whitelist,true));
-			//sort($model->whitelist);
 			$model->whitelist = json_encode($model->whitelist);
         	$model->save();
             return $this->redirect(['update']);
