bf91b7621064203505a80a4d875144094066e67b
tdasto <tdasto@gmail.com>
2021-12-29 15:50:39 -0500

    Add Date Range & Export
    For #172

--- backend/models/search/CardReceiptSearch.php
+++ backend/models/search/CardReceiptSearch.php
@@ -63,13 +63,15 @@ class CardReceiptSearch extends CardReceipt {
         }
 
         // grid filtering conditions
-        $query->andFilterWhere([
+        if(isset($this->tx_date)) {
+			$query->andWhere($this->getWhere($this->tx_date,'tx_date'));
+		}
+		$query->andFilterWhere([
             'id' => $this->id,
         ]);
 		
 		if(isset($this->badge_number)) { $query->andFilterWhere(['like', 'badge_number', $this->badge_number]); }
-        if(isset($this->tx_date)) { $query->andFilterWhere(['like', 'tx_date', $this->tx_date]); }
-		if(isset($this->cart)) { $query->andFilterWhere(['like', 'cart', $this->cart]); }
+        if(isset($this->cart)) { $query->andFilterWhere(['like', 'cart', $this->cart]); }
 		if(isset($this->name)) { $query->andFilterWhere(['like', 'name', $this->name]); }
 		if(isset($this->amount)) { $query->andFilterWhere(['like', 'amount', $this->amount]); }
 		if(isset($this->tx_type)) { $query->andFilterWhere(['like', 'tx_type', $this->tx_type]); }
@@ -87,5 +89,26 @@ class CardReceiptSearch extends CardReceipt {
 		
 //yii::$app->controller->createLog(true, 'trex-b-m-s-crs', 'Raw Sql: '.var_export($query->createCommand()->getRawSql(),true));
         return $dataProvider;
-    }
+	}
+
+	private function getWhere($mydate,$field) {
+		$two=false;
+		$query='';
+		if(isset($mydate) && $mydate!='') {
+			$two=strpos($mydate,'-');
+			if($two) {
+				if(strtotime(substr($mydate,0,10)) == strtotime(substr($mydate,13,23))) {
+					$query=" ".$field." like '".date('Y-m-d',strtotime(substr($mydate,0,10)))."%'";
+				} else {
+					$query=" ".$field." >= '".date('Y-m-d',strtotime(substr($mydate,0,10)))." 00:00' AND ".
+					            "".$field." <= '".date('Y-m-d',strtotime(substr($mydate,13,23)))." 23:59'";
+				}
+			} else {
+				$query=" ".$field." like '".date('Y-m-d',strtotime($mydate))."%'";
+			}
+		} else {
+			$query=" ".$field." like '".date('Y-m-d',strtotime(yii::$app->controller->getNowTime()))."%'";
+		}
+		return $query;
+	}
 }
--- backend/views/sales/purchases.php
+++ backend/views/sales/purchases.php
@@ -1,5 +1,6 @@
 <?php
 
+use kartik\daterange\DateRangePicker;
 use yii\helpers\Html;
 use yii\grid\GridView;
 use kartik\widgets\ActiveForm;
@@ -12,6 +13,7 @@ use kartik\export\ExportMenu;
 $this->title = 'Purchases';
 $this->params['breadcrumbs'][] = ['label' => 'Store', 'url' => ['index']];
 
+if (isset($_REQUEST['sales-showsku'])) {$showsku= true;} else {$showsku= false;}
 if (isset($_REQUEST['CardReceiptSearch']['pagesize'])) { 
 	$pagesize = $_REQUEST['CardReceiptSearch']['pagesize']; 
 	$_SESSION['pagesize'] = $_REQUEST['CardReceiptSearch']['pagesize'];
@@ -36,7 +38,8 @@ echo $this->render('_view-tab-menu').PHP_EOL ?>
 
 <div class="row">
 
-    <div class="col-xs-12"> <?php
+    <div class="col-xs-12"> 
+	<?php
 	$gridColumns = [
 		[
 			'attribute'=>'badge_number',
@@ -51,12 +54,13 @@ echo $this->render('_view-tab-menu').PHP_EOL ?>
 		[
 			'attribute'=>'cart',
 			'format' => 'raw',
-			'value' => function($model, $attribute) {
+			'value' => function($model, $attribute) use ($showsku) {
 				$items='';
 				$dcart = json_decode($model->cart);
 				if(is_array($dcart)){
 				foreach($dcart as &$item ) {
 					if(isset($item) && isset($item->item)){
+					if($showsku) {$items .=$item->sku.' - ';}
 					$items .=$item->item.' ['.$item->ea.' x '.$item->qty.'] '.($item->price)."<br/>\n";
 					}
 				}}
@@ -66,10 +70,13 @@ echo $this->render('_view-tab-menu').PHP_EOL ?>
 		],
 		[
 			'attribute'=>'amount',
+			'headerOptions' => ['width' => '100'],
 			'footer' => "$".number_format($dataProvider->query->sum('amount'), 2, '.', ','),
 		],
 		[ 
 			'attribute'=>'cashier',
+			'label' => 'Cashier (Use commas to seperate multiple Cashiers when filtering.',
+			'headerOptions' => ['width' => '300'],
 			//'captionOptions' => ['tooltip' => 'test test',]
 		],
 		'tx_type',
@@ -89,24 +96,23 @@ echo $this->render('_view-tab-menu').PHP_EOL ?>
 			]
 		]
 	];
-?>
+	?>
 		
 <div class="sales-search">
 <div class="row">
 	<div class="col-xs-6 col-sm-4">
 		<h1><?= Html::encode($this->title) ?></h1>
 	</div>
+</div>
 
 	<?php $form = ActiveForm::begin([
 		'action' => ['purchases'],
 		'method' => 'get',
 		'id'=>'viewSalsetFilter',
 	]); ?>
-	<div class="col-xs-6 col-sm-3">
-		Use a comma (,) to seperate multiple Cashiers when filtering.
-		<?php //= $form->field($model, 'atRange_condition')->dropDownlist(['all'=>'All','atRange'=>'At Range','gone'=>'Past Visitors'],['value'=>$model->atRange_condition !=null ? $model->atRange_condition : 'atRange'])->label('Fliter by') ?>
-	</div>
-	<div class="col-xs-2 col-sm-1" > <p> <br /></p>
+<div class="row">
+	<div class="col-xs-4 col-sm-2" > <p> <br /></p>
+		Export Data - 
 		<?=ExportMenu::widget([
 			'dataProvider' => $dataProvider,
 			'filterModel' => $searchModel,
@@ -124,19 +130,33 @@ echo $this->render('_view-tab-menu').PHP_EOL ?>
 			]
 		]) . "<br /> <br />\n";?>
 	</div>
+	<div class="col-xs-3 col-sm-3">	
+<br>	<?php echo Html::checkbox('sales-showsku',$showsku,['value'=>1,'id'=>'sales-showsku']), PHP_EOL; ?><b> - Show SKU</b></p>
+	</div>
+	<div class="col-xs-3 col-sm-3" "btn-group pull-right">	
+	<?=$form->field($searchModel, 'tx_date', [
+		'options'=>['class'=>'drp-container form-group']
+		])->widget(DateRangePicker::classname(), [
+			'presetDropdown'=>true,
+			'hideInput'=>false,
+			'pluginOptions' => [
+				'opens'=>'left',
+				'locale'=>['format'=>'MM/DD/YYYY'],
+			]])->label('Date range:'); ?>
+	</div>
 	<div class="col-xs-3 col-sm-2">
 		<?= $form->field($searchModel, 'pagesize')->dropDownlist([ 20 => 20, 50 => 50, 100 => 100, 200=>200],['value'=>$pagesize ,'id' => 'pagesize'])->label('Page size: ') ?>
 	</div>
-	<div class="col-xs-6 col-sm-2">
+	<div class="col-xs-3 col-sm-2">
 		<div class=" form-group btn-group ">
 		<br>
 			<?= Html::submitButton('<i class="fa fa-search" aria-hidden="true"></i> Search', ['class' => 'btn btn-primary']) ?>
 			<?= Html::a('<i class="fa fa-eraser" aria-hidden="true"></i> Reset', ['purchases?reset=true'],['class' => 'btn btn-danger']) ?>
 		</div>
 	</div>
+</div>
 
 	<?php ActiveForm::end(); ?>
-</div>
 </div>
 		
 		<?php
