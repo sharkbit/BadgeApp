8a57a4e78171001aedc3b57b22f963a16615a3b8
Shark <sharkbit@hotmail.com>
2022-05-30 20:12:28 -0400

    Better Wording
    for #219

--- backend/views/sales/index.php
+++ backend/views/sales/index.php
@@ -209,7 +209,7 @@ echo $this->render('_view-tab-menu').PHP_EOL; ?>
 						$item_qty =	'<input class="right" type="text" name="qty" size="3" value='.$guest_total.' onKeyUp="doCalcSale()" />';
 					} else {
 						$item_qty='<input class="right" type="text" name="qty" size="3" value=0 disabled />';
-						$guest_note=' - <a href="/guest/index">  <B>Missing Guest Count</a>';
+						$guest_note=' - <a href="/guest/index">  <B><u>Use Pay Now Option<u></a>';
 					}
 				}
 
